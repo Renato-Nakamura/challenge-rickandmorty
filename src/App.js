cd0f2d0dd46f6df6f80392fdda37b3381a1949e1
Renato-Nakamura <renato.yukio30@hotmail.com>
2021-10-03 07:23:53 -0300

    generic modal

--- src/App.js
+++ src/App.js
@@ -2,8 +2,17 @@ import './App.css';
 import Card from './components/cards/Cards';
 import styled from "styled-components";
 import { gql,useQuery } from '@apollo/client';
+import 'react-responsive-modal/styles.css';
+import { Modal } from 'react-responsive-modal';
+import { useState } from 'react';
 
 function App() {
+  //modal
+  const [open, setOpen] = useState(false);
+
+  const onOpenModal = () => setOpen(true);
+  const onCloseModal = () => setOpen(false);
+  //modal
   
   const CHARACTERS = gql`
   query {
@@ -21,7 +30,6 @@ function App() {
     }
   }
   `
-  
   const {loading, error, data} = useQuery(CHARACTERS)
   if (loading) return null;
   if (error) return `Error! ${error}`;
@@ -37,12 +45,16 @@ function App() {
       <main>
       {characters.map(character => {
         return (<Card 
+        onClick={() => onOpenModal(character.id)}
         img={character.image}
         title={character.name}
         description= {character.status}
       />)
       })}
       </main>
+      <Modal open={open} onClose={onCloseModal} center>
+        <h2>Modal limpo</h2>
+      </Modal>
     </div>
   );
 }
